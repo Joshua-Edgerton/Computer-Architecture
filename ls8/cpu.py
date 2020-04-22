1908152178fc684b15b118321a29bee3dce9274e
Joshua Edgerton <joshuaxedgerton@gmail.com>
2020-04-21 19:11:45 -0500

    Took out hardcodes, implemented load function

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -23,33 +23,56 @@ class CPU:
     def ram_write(self, write_value, address):
         self.ram[address] = write_value
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
+        try:
+            address = 0
+            # Open the file
+            with open(sys.argv[1]) as f:
+                # Read through lines
+                for line in f:
+                    # Iterate through comments
+                    comment_split = line.strip().split("#")
+                    # Cast number string to int
+                    value = comment_split[0].strip()
+                    # Ignore blank lines
+                    if value == "":
+                        continue
+                    instruction = int(value, 2)
+                    # Add to memory array
+                    self.ram[address] = instruction
+                    address += 1      
+
+        except:
+            print("cant find file")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
